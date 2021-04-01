4ce13d114278b61d0a7f35f15aff5d1ee010a7aa
John Orji <john.orji@cognizant.com>
2021-04-01 16:05:38 -0500

    added config service and setup environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EnemyApiService } from './services/enemy-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { EnemyApiService } from './services/enemy-api.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [EnemyApiService],
   bootstrap: [AppComponent]
---
+++ src/app/services/config/config.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Config } from 'protractor';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  private readonly configPath: string;
+
+  constructor(private readonly http: HttpClient) {
+    this.configPath = environment.config;
+  }
+
+  get(): Observable<Config> {
+    return this.http.get<Config>(this.configPath);
+  }
+}
