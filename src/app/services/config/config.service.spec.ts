726850a37ebc628aa25b6ff4e1eda2f90f88f9bc
John Orji <john.orji@cognizant.com>
2021-06-01 15:00:08 -0500

    added necessary http dependencies to tests

--- src/app/services/config/config.service.spec.ts
+++ src/app/services/config/config.service.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ConfigService } from './config.service';
 
 describe('ConfigService', () => {
   let service: ConfigService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    
     service = TestBed.inject(ConfigService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get the correct path', () => {
+    service.get().subscribe((res) => {
+      expect(res.baseUrl).toEqual("http://localhost:8081/");
+    })
   });
 });
--- src/app/services/enemy-api.service.spec.ts
+++ src/app/services/enemy-api.service.spec.ts
@@ -1,12 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EnemyApiService } from './enemy-api.service';
 
 describe('EnemyApiService', () => {
   let service: EnemyApiService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     service = TestBed.inject(EnemyApiService);
   });
 
