9eb7e28e8a857c7182f029104f3f501025cc0533
John Orji <john.orji@cognizant.com>
2021-06-01 16:19:58 -0500

    added vscode to gitignore properly, also trying out this testingcontroller thing

--- src/app/services/config/config.service.spec.ts
+++ src/app/services/config/config.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 
 import { ConfigService } from './config.service';
 
@@ -9,13 +10,15 @@ describe('ConfigService', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
+  const CONFIG = environment.config;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
-    
+
     service = TestBed.inject(ConfigService);
   });
 
@@ -24,8 +27,13 @@ describe('ConfigService', () => {
   });
 
   it('should get the correct path', () => {
+    let req: TestRequest;
+
     service.get().subscribe((res) => {
       expect(res.baseUrl).toEqual("http://localhost:8081/");
-    })
+    });
+
+    req = httpTestingController.expectOne(CONFIG);
+    req.flush("");
   });
 });
