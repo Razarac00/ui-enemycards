cc83bbe7ad0d1891923575e7054332ccb4653f2c
John Orji <john.orji@cognizant.com>
2021-06-03 16:11:47 -0400

    basic setup for the service request tests

--- src/app/services/enemy-api.service.spec.ts
+++ src/app/services/enemy-api.service.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EnemyApiService } from './enemy-api.service';
@@ -8,10 +8,12 @@ describe('EnemyApiService', () => {
   let service: EnemyApiService;
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let req: TestRequest;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      //TODO: add a provider for the config service to mock the get method
     });
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
@@ -23,5 +25,25 @@ describe('EnemyApiService', () => {
     expect(service).toBeTruthy();
   });
 
-  
+  it('should get all enemies', () => {
+    let search = "";
+    let pageNumber = 0;
+    let pageSize = 4;
+    service.getAllEnemies(search, pageNumber, pageSize).subscribe((res) => {
+      expect(res).toBeTruthy();
+    }); 
+    //  http://localhost:8081/api/enemies?search=G&pageNumber=0&pageSize=4
+    req = httpTestingController.expectOne("");
+    req.flush("");
+  });
+
+  it('should get an enemy', () => {
+    let name = "Nito";
+    service.getEnemy(name).subscribe((res) => {
+      expect(res).toBeTruthy();
+    }); 
+    //  http://localhost:8081/api/enemies/Bell-Gargoyle
+    req = httpTestingController.expectOne("");
+    req.flush("");
+  });
 });
